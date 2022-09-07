3802d6b2a94b720997caeadea96dd521316c416e
Akash <aakashkatiyar60716@gmail.com>
2022-09-07 22:32:24 +0530

    Add files

---
+++ src/util/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<h4>
+				Built by &nbsp;<a href='https://github.com/AkashKatiyar2509'>Akash Katiyar</a>
+			</h4>
+		</footer>
+	);
+};
+
+export default Footer;
